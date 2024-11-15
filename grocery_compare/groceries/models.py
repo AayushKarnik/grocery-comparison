629ed384c13a95d425cae102dfe7135d076ef119
Aayush Karnik <karnikayush@gmail.com>
2024-11-15 13:59:30 +0530

    updating css

--- grocery_compare/groceries/models.py
+++ grocery_compare/groceries/models.py
@@ -36,3 +36,10 @@ class UserPreference(models.Model):
     def __str__(self):
         return f"{self.user}'s Preferences"
 
+from django.db import models
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
--- grocery_compare/groceries/views.py
+++ grocery_compare/groceries/views.py
@@ -4,6 +4,41 @@ from .models import Product, Price, UserPreference
 
 # groceries/views.py
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from .models import Product, Review
+
+def submit_review(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        rating = request.POST['rating']
+        comment = request.POST['comment']
+        Review.objects.create(product=product, rating=rating, comment=comment)
+    return redirect('product_detail', product_id=product.id)
+
+from django.core.paginator import Paginator
+
+
+def product_search(request):
+    query = request.GET.get('query', '')
+    max_price = request.GET.get('max_price')
+    location = request.GET.get('location')
+    time_filter = request.GET.get('time_filter')
+
+    products = Product.objects.filter(name__icontains=query)
+    prices = Price.objects.filter(product__in=products)
+
+    if max_price:
+        prices = prices.filter(price__lte=max_price)
+    if location:
+        prices = prices.filter(location=location)
+    if time_filter:
+        prices = prices.filter(availability_date__gte=time_filter)
+
+    paginator = Paginator(prices, 10)  # Show 10 items per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'groceries/product_search.html', {'page_obj': page_obj, 'query': query})
+
 
 def home(request):
     return render(request, "groceries/home.html")
